ee0c250006c188f0f462ba112ce3014867177d22
AuthoredEntropy <unconfigured@null.spigotmc.org>
2020-06-17 01:27:47 -0600

    launch file auto git

--- discord_js_bot_main/launch.bat
+++ discord_js_bot_main/launch.bat
@@ -1,5 +1,6 @@
 git config --global user.name "AuthoredEntropy"
-git pull
+git fetch origin master
+git reset --hard FETCH_HEAD
 node C:\Users\Administrator\Desktop\bot\WhiteLightningServers\discord_js_bot_main\index.js "C://Users//Administrator//Desktop//bot//WhiteLightningServers//discord_js_bot_main" "C://Users//Administrator//Desktop//bot//WhiteLightningServers//JavaBuilds"
 pause
 
