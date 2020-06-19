f7c402f39608dd9d53148dee3a88ac201bff90b8
AuthoredEntropy <unconfigured@null.spigotmc.org>
2020-06-19 10:11:14 -0600

    buch o stuff

--- discord_js_bot_main/launch - Copy.bat
+++
@@ -1,3 +0,0 @@
-node C:\Users\mmful\Desktop\discord bot\WhiteLightningServers\discord_js_bot_main\index.js "C:\\Users\\mmful\\Desktop\\discord bot\\WhiteLightningServers\\discord_js_bot_main" "C:\\Users\\mmful\\Desktop\\discord bot\\WhiteLightningServers\\JavaBuilds" client none localhost 7777
-pause
-
