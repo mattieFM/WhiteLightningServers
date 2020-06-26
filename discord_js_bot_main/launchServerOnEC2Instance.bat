75661d0a19f76c539ba14212fe30de4f235338f3
AuthoredEntropy <unconfigured@null.spigotmc.org>
2020-06-26 17:21:39 -0600

    ge

--- discord_js_bot_main/launchServerOnEC2Instance.bat
+++ discord_js_bot_main/launchServerOnEC2Instance.bat
@@ -1,3 +1,3 @@
-node C:\Users\mmful\Desktop\discord bot\WhiteLightningServers\discord_js_bot_main\index.js "C:\\Users\\Administrator\\Desktop\\bot\\WhiteLightningServers\\discord_js_bot_main" "C:\\Users\\mmful\\Desktop\\discorbot\\WhiteLightningServers\\JavaBuilds" server internalTestIngId localhost 7777
+node C:\Users\Administrator\Desktop\bot\WhiteLightningServers\discord_js_bot_main\index.js "C:\\Users\\Administrator\\Desktop\\bot\\WhiteLightningServers\\discord_js_bot_main" "C:\\Users\\mmful\\Desktop\\discorbot\\WhiteLightningServers\\JavaBuilds" server internalTestIngId localhost 7777
 pause
 
