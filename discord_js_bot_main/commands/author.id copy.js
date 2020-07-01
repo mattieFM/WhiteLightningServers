bff358dd3af16b6021dbe0d1d9fc9df7e4ccee8e
AuthoredEntropy <unconfigured@null.spigotmc.org>
2020-07-01 16:55:16 -0600

    ADWADAWDADWADAWAREFGR

--- discord_js_bot_main/commands/author.id copy.js
+++ discord_js_bot_main/commands/author.id copy.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 const Config = require("../config_auth/Config.json");
 
 module.exports.run = async(bot, message, args) => {
-    (args, bot, message) => {
     // console.log(message.author.id);
     // console.log(Config.ownerID);
     if(message.author.id != Config.ownerID) return;
@@ -22,27 +21,29 @@ module.exports.run = async(bot, message, args) => {
 let instance;
 const AWS = require("aws-sdk");
 
-new AWS.EC2({apiVersion: '2016-11-15'}).describeInstances({
-    Filters: [
-        {
-       Name: "tag:Name", 
-       Values: [
-          args[0]
-       ]
-      }
-     ]
-    }, (err, data) =>{
-        if(err){
+let aws = new AWS.EC2({apiVersion: '2016-11-15'})
+
+    aws.describeInstances({
+        Filters: [
+            {
+                Name: "tag:Name",
+                Values: [
+                    args[0]
+                ]
+            }
+        ]
+    }, (err, data) => {
+        if (err) {
             console.log("Error", err.stack);
-        } else {
-               console.log(data.Reservations[0].Instances[0]);
-               message.channel.send("public ip: " + data.Reservations[0].Instances[0].PublicIpAddress);
-               message.channel.send("KeyName: " + data.Reservations[0].Instances[0].KeyName);
-               message.channel.send("ImageId: " + data.Reservations[0].Instances[0].ImageId);
-               message.channel.send("InstanceId: " + data.Reservations[0].Instances[0].InstanceId);
+        }
+        else {
+            console.log(data.Reservations[0].Instances[0]);
+            message.channel.send("public ip: " + data.Reservations[0].Instances[0].PublicIpAddress);
+            message.channel.send("KeyName: " + data.Reservations[0].Instances[0].KeyName);
+            message.channel.send("ImageId: " + data.Reservations[0].Instances[0].ImageId);
+            message.channel.send("InstanceId: " + data.Reservations[0].Instances[0].InstanceId);
         }
     });
-}
 }
 
 module.exports.help = {
