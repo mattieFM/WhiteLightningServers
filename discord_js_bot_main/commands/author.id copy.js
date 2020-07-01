af348e989af07b26eaba88ed88ddfde176585c47
AuthoredEntropy <unconfigured@null.spigotmc.org>
2020-07-01 16:18:41 -0600

    eate0

---
+++ discord_js_bot_main/commands/author.id copy.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const Config = require("./Config.json");
+
+module.exports.run = async(bot, message, args) => {
+    let Params ={
+        Filters: [
+            {
+           Name: "tag:Name", 
+           Values: [
+              "J5JvA2Nu38ZzLxvCrkmqJ6EvCLuimureqpE1cErfcE9QcacCVjy9D1NoCS3mYXa0brW32cuIuzT9kCHb5FvkUbrPE3JunO05oMzVfsFiyvjmT06arZvHDQt28yH6blqHnZixw4AiqoN9ftDlooVTF2kDpTJM0pgO2Pztfc9sEUk9DnvERMT9EHkdt79gcUIBHTbBT8LN"
+           ]
+          }
+         ]
+        };
+let controller = require("./Ec2Controller").ec2launch;
+let instance;
+const AWS = require("aws-sdk");
+
+new AWS.EC2({apiVersion: '2016-11-15', region: Ec2Request.zone}).describeInstances(Params, (err, data) =>{
+        if(err){
+            console.log("Error", err.stack);
+        } else {
+               console.log(JSON.stringify(data));
+        }
+    });
+}
+
+module.exports.help = {
+    name: "testttttt"
+}
