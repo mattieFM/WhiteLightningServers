cb61f153c8818f600fafb338a55334e84fe3f363
AuthoredEntropy <unconfigured@null.spigotmc.org>
2020-07-02 14:36:48 -0600

    rfaefeaf3a

--- discord_js_bot_main/launchclientOnEC2Instance.bat
+++ discord_js_bot_main/launchclientOnEC2Instance.bat
@@ -1,6 +1,6 @@
 git config --global user.name "AuthoredEntropy"
 git fetch origin master
 git reset --hard FETCH_HEAD
-node C:\Users\Administrator\Desktop\bot\WhiteLightningServers\discord_js_bot_main\index.js "C:\Users\Administrator\Desktop\bot\WhiteLightningServers\discord_js_bot_main" "C:\\Users\\Administrator\\Desktop\\bot\\WhiteLightningServers\\JavaBuilds" client %1 %2 %3
+node C:\Users\Administrator\Desktop\bot\WhiteLightningServers\discord_js_bot_main\index.js "C:\Users\Administrator\Desktop\bot\WhiteLightningServers" "C:\\Users\\mmful\\Desktop\\discorbot\\WhiteLightningServers\\JavaBuilds" client %1 %2 %3
 pause
 
