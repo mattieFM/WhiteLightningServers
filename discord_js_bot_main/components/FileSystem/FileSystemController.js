1d99cb3d399fd91477f1448ce90c2edce999cbe9
AuthoredEntropy <unconfigured@null.spigotmc.org>
2020-07-07 16:27:25 -0600

    fixed promise

--- discord_js_bot_main/components/FileSystem/FileSystemController.js
+++ discord_js_bot_main/components/FileSystem/FileSystemController.js
@@ -523,21 +523,23 @@ module.exports.FileSystemController = class FileSystemControllerserver {
     }
   }
   async FindOrCreateUserInfoFromServerRequest(ServerRequest) {
-    new Promise(async resolve => {
-      let UserInfo;
+    return new Promise(async resolve => {
+      
       //checking if user has any info file created,
       if ((await this.CheckUserInfoByID(ServerRequest.OwnerID)) === true) {
-        UserInfo = await this.GetUserInfoByID(ServerRequest.OwnerID);
+        let UserInfo = await this.GetUserInfoByID(ServerRequest.OwnerID);
+        resolve(UserInfo);
         //if it does, it will grab the UserInfo file
       } else {
         //if the user does not have a UserInfo file one will be created
         //currently using random Patron Teir before that is implemented
-        UserInfo = await this.CreateUserInfoFromServerRequest(
+        let UserInfo = await this.CreateUserInfoFromServerRequest(
           ServerRequest,
           Math.floor(Math.random() * 4)
         );
+        resolve(UserInfo);
       }
-      resolve(UserInfo);
+      
     })
     
   }
